b188d939119a6c42b2ed973852869b5e2c5b2b02
Dispersia <dispersias@gmail.com>
2015-02-27 12:42:36 -0700

    Maybe fix nullreference?

--- SnapchatDesktop/Client.cs
+++ SnapchatDesktop/Client.cs
@@ -24,9 +24,10 @@ namespace SnapchatDesktop
         public static string AuthToken { get; set; }
         public static string RequestToken { get; set; }
         private const string
+            SnapVersion = "9.1.2.0",
             BaseUrl = "https://feelinsonice-hrd.appspot.com/loq/",
             Static_Token = "m198sOkJEn37DjqZ32lpRu76xmw288xSQ9",
-            User_Agent = "Snapchat/9.1.2.0 (Nexus 4; Android 18; gzip)",
+            User_Agent = "Snapchat/" + SnapVersion + " (Nexus 4; Android 18; gzip)",
             Secret_Token = "iEk21fuwZApXlz93750dmW22pw389dPwOk",
             Pattern = "0001110111101110001111010101111011010001001110011000110001000110";
 
@@ -65,24 +66,14 @@ namespace SnapchatDesktop
         {
             TimeSpan span = DateTime.Now - new DateTime(1970, 1, 1);
             string timestamp = (span.TotalSeconds * 1000).ToString(CultureInfo.InvariantCulture).Substring(0, 13);
-            string first = getSha(Secret_Token + Static_Token);
-            string second = getSha(timestamp + Secret_Token);
-
-            StringBuilder tokenBuilder = new StringBuilder();
-            for (int i = 0; i < Pattern.Length; i++)
-            {
-                char c = Pattern[i];
-                tokenBuilder.Append(c == '0' ?
-                    first[i] :
-                    second[i]);
-            }
+            
 
             var postData = new Dictionary<string, string>()
             {
                 { "username", username },
                 { "password", password },
                 { "timestamp", timestamp },
-                { "req_token", tokenBuilder.ToString() },
+                { "req_token", getToken(timestamp, Static_Token) },
                 { "features_map", "{\"all_updates_friends_response\":true}" },
                 { "access_token", "eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0ZWIyNDY1MGE0NzViNDkzZGQzZjFiMjU2MmM5MTZmOTA1MzIyOTAifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwic3ViIjoiMTA0NzkzMDIyMzY2MTc0NTE1NTAxIiwiYXpwIjoiNjk0ODkzOTc5MzI5LXFnMGkwdTg4dDBobThrNmsxbWJyYm5zdWoxMDFoNzN2LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwiZW1haWwiOiJkaXNwZXJzaWFzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdWQiOiI2OTQ4OTM5NzkzMjktbDU5ZjNwaGw0MmV0OWNscG9vMjk2ZDhyYXFvbGpsNnAuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJpYXQiOjE0MjUwMjE3OTAsImV4cCI6MTQyNTAyNTY5MH0.K22ML2yd3OK-2hdQzzcykmrzlG1mdfWGd9USxPnTBWSj01EDYRp2IrtiKLurSUuHwOrhUUzflyQCaWwH7lH6935U_jh6OA-LHUielGWZYcgKMqUY0I56AhHmYoigUmydVj05F6BAvnJ_seEL5gm4nk7HEZSA-__QPagntdf6Knk" } //Need to decompile again to figure this one out.
             };
@@ -107,13 +98,29 @@ namespace SnapchatDesktop
             {
                 MySnapchat = new Snapchat.Snapchat();
                 MySnapchat.UpdateResponse.Logged = false;
-                //MySnapchat.updateResponse. = "401 Unauthorized error. Please submit as an issue on github, thanks!"
+                MySnapchat.Message = "401 Unauthorized error. Please submit as an issue on github, thanks!";
             }
 
             if (!MySnapchat.UpdateResponse.Logged)
                 return false;
 
             return true;
+        }
+
+        private static string getToken(string timestamp, string one)
+        {
+            string first = getSha(Secret_Token + one);
+            string second = getSha(timestamp + Secret_Token);
+
+            StringBuilder tokenBuilder = new StringBuilder();
+            for (int i = 0; i < Pattern.Length; i++)
+            {
+                char c = Pattern[i];
+                tokenBuilder.Append(c == '0' ?
+                    first[i] :
+                    second[i]);
+            }
+            return tokenBuilder.ToString();
         }
 
         private static string getSha(string input)
