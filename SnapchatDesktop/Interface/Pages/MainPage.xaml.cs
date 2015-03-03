83c6cb854e3f5d9a540416599cfcc07de5362dc8
Dispersia <dispersias@gmail.com>
2015-03-03 01:31:06 -0700

    Need to test on laptop.

--- SnapchatDesktop/Interface/Pages/MainPage.xaml.cs
+++ SnapchatDesktop/Interface/Pages/MainPage.xaml.cs
@@ -3,9 +3,14 @@ using SnapchatDesktop.Interface.Controls;
 using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-using System.Diagnostics;
 using System.Windows;
-using SnapchatHelper.JSONObjects.bq;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
 
 namespace SnapchatDesktop.Pages
 {
@@ -14,9 +19,16 @@ namespace SnapchatDesktop.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        private VideoCaptureDevice webcam;
+        private FilterInfoCollection webcamCollection;
+
         public MainPage()
         {
             InitializeComponent();
+            webcamCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            webcam = new VideoCaptureDevice(webcamCollection[0].MonikerString);
+            webcam.NewFrame += Webcam_NewFrame;
+
             if (Client.Snapchat.friendsResponse.Friends != null)
             {
                 Client.Snapchat.friendsResponse.Friends = Client.Snapchat.friendsResponse.Friends.OrderBy(x =>
@@ -51,6 +63,24 @@ namespace SnapchatDesktop.Pages
                     friendsListBox.Items.Add(item);
                 }
             }
+        }
+
+        private void Webcam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            System.Drawing.Image image = (Bitmap) eventArgs.Frame.Clone();
+            MemoryStream mStream = new MemoryStream();
+            image.Save(mStream, ImageFormat.Bmp);
+            mStream.Seek(0, SeekOrigin.Begin);
+            BitmapImage bImage = new BitmapImage();
+            bImage.BeginInit();
+            bImage.StreamSource = mStream;
+            bImage.EndInit();
+
+            bImage.Freeze();
+            Dispatcher.BeginInvoke(new ThreadStart(delegate
+            {
+                webcamCapture.Source = bImage;
+            }));
         }
 
         private async void CaptureButton_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
